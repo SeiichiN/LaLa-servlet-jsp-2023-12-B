bd65071f7eb36f043dc074526cc78c4854dac289
SeiichiN <billie175@gmail.com>
2024-01-10 16:34:06 +0900

    EmployeeDAO modified

--- chap13/src/main/java/dao/EmployeesDAO.java
+++ chap13/src/main/java/dao/EmployeesDAO.java
@@ -12,7 +12,7 @@ import model.Employee;
 
 public class EmployeesDAO {
 	private final String JDBC_URL = 
-			"jdbc:h2:tcp://localhost/~/example";
+			"w";
 	private final String DB_USER = "sa";
 	private final String DB_PASS = "";
 			

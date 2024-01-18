c62ddf4c29f02245f0d7c62ce7cdd731cde78d4d
SeiichiN <billie175@gmail.com>
2024-01-19 08:19:44 +0900

    about_class_loader modified.

--- about_class_loader/src/load_static/Main.java
+++ about_class_loader/src/load_static/Main.java
@@ -3,8 +3,20 @@ package load_static;
 public class Main {
 
 	public static void main(String[] args) {
+		// MyUtilクラスのstaticメソッドを実行すると、
+		// MyUtilクラスがクラスローダによってメモリ上にロードされる。
 		int num = MyUtil.random(10);
+		// MyUtilクラスのクラス情報を clazzを使って取得できる。
 		Class<?> clazz = MyUtil.class;
+		// ここではクラスローダを調べている。
+		System.out.println(clazz.getClassLoader());
+		caseString();
+	}
+	
+	public static void caseString() {
+		System.out.println("----- case String -----");
+		String hum = "hum";
+		Class<?> clazz = hum.getClass();
 		System.out.println(clazz.getClassLoader());
 	}
 
